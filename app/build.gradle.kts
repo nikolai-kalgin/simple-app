3ebd3d81c9e1059401aa89807fccfaf8431f85e3
Nikolai Kalgin <nikolai.kalgin@jetbrains.com>
2025-11-26 16:48:47 +0200

    format

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -19,6 +19,7 @@ dependencies {
     // Use JUnit Jupiter for testing.
     testImplementation(libs.junit.jupiter)
 
+
     testRuntimeOnly("org.junit.platform:junit-platform-launcher")
 
     // This dependency is used by the application.
